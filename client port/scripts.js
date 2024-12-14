fc98aac49ce9173418db2bf22415f9eb6a2c7076
Hemanathan R <161623586+Hemanathan2005@users.noreply.github.com>
2024-12-14 12:01:37 +0530

    Add files via upload

---
+++ client port/scripts.js
@@ -0,0 +1,11 @@
+// For smooth scroll effect on anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
